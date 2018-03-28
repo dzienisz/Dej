df72372facfc450ced8d25f89f86daf902542dab
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2018-03-28 13:44:42 +0200

    folder structure 📂

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
