afa1af0bd2534d90f5cd70a21ec74b492a11cd10
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2018-03-28 14:51:13 +0200

    add react-router 🛣

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import { Router, Route, browserHistory } from 'react-router';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Router path="/" history={browserHistory}>
+        <Route path="/app" component={App} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+    </Router>,
+    document.getElementById('root'));
 registerServiceWorker();
