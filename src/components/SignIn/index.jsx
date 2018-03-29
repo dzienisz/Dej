da20d882444eba1076d80039766a7837765090d7
Kamil Dzieniszewski <kamil.dzieniszewski@gmail.com>
2018-03-29 11:20:03 +0200

    add signin component 👆

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { firebaseApp } from '../../firebase';
 import './style.css';
 
 class SignIn extends Component {
-  render() {
-    return (
-      <div className="SignIn">
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: {
+                message: '',
+            }
+        }
+    }
+
+    signIn() {
+        console.log('this.state', this.state);
+        const { email, password } = this.state;
+        firebaseApp.auth().signInWithEmailAndPassword(email, password)
+            .catch(error => {
+                this.setState({error})
+            })
+    }
+
+    render() {
+        return (
+            <div className="form-inline" style={{margin: '5%'}}>
+                <h2>Sign In</h2>
+                <div className="form-group">
+                    <input
+                        className="form-control"
+                        type="text"
+                        style={{marginBottom: '5px', marginRight: '5px'}}
+                        placeholder="email"
+                        onChange={event => this.setState({email: event.target.value})}
+                    />
+                    <input
+                        className="form-control"
+                        type="password"
+                        style={{marginBottom: '5px', marginRight: '5px'}}
+                        placeholder="password"
+                        onChange={event => this.setState({password: event.target.value})}
+                    />
+                    <button
+                        className="btn btn-primary"
+                        type="button"
+                        style={{marginBottom: '5px'}}
+                        onClick={() => this.signIn()}
+                    >
+                        Sign In
+                    </button>
+                </div>
+                <div>{this.state.error.message}</div>
+            </div>
+        );
+    }
 }
 
 export default SignIn;
