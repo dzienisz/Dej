1b93ad80f8a4f7abc1737fab6ac45632618f699f
Kamil Dzieniszewski <kamil.dzieniszewski@vodeno.com>
2019-09-08 18:22:42 +0200

    Updates

---
+++ precache-manifest.a50f058019eb950fd75e392ef96120ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3729f202840f0627f8944a703438d4ac",
+    "url": "/Dej/index.html"
+  },
+  {
+    "revision": "f6a2dfc78417f176fc2f",
+    "url": "/Dej/static/css/main.9ffdf795.chunk.css"
+  },
+  {
+    "revision": "89affa8ba2a5bb47ac1b",
+    "url": "/Dej/static/js/2.168626dc.chunk.js"
+  },
+  {
+    "revision": "f6a2dfc78417f176fc2f",
+    "url": "/Dej/static/js/main.b518db86.chunk.js"
+  },
+  {
+    "revision": "cbe10a9aa6c3818def22",
+    "url": "/Dej/static/js/runtime~main.7de9ef2d.js"
+  }
+]);
